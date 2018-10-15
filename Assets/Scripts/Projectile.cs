8cee58bda556a10ddc4a77b808ea69247888ae35
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2018-10-15 15:56:20 +0100

    Implemented initial form of projectile bounce limit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody projectileRB;
+    private int m_remainingBounces = 5;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        m_remainingBounces--;
+        if (m_remainingBounces <= 0)
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log(gameObject + " - Remaining Bounces: " + m_remainingBounces);
+    }
+}
