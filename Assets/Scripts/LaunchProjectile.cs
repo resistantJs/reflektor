b4b0740892d055b3c8064bf32b19f76bb5e302ed
Rowan Lovejoy <rlovejoy1@socnet.soc.plymouth.ac.uk>
2018-10-15 15:20:18 +0100

    Added test level, projectile launcher and projectile, basic launching functionality

---
+++ Assets/Scripts/LaunchProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour
+{
+    public Rigidbody projectilePrefab;
+    public Transform projectileStart;
+    public float launchVelocity = 5000.0f;
+
+    private Transform m_launchDirection;
+    
+
+    private void Awake()
+    {
+        m_launchDirection = projectileStart.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectilePrefab, projectileStart.position, projectileStart.rotation) as Rigidbody;
+
+            projectileInstance.AddForce(projectileStart.forward * launchVelocity);
+        }
+	}
+}
