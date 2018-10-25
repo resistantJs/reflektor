c9ca4bb993603e2c41726807cc4429b71026be91
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2018-10-25 09:23:18 +0100

    Added score target and basic scoring system

---
+++ Assets/Code/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public GameObject txtScore;
+
+    private int m_score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public void AddToScore(int _amount)
+    {
+        if (_amount > 0)
+        {
+            m_score += _amount;
+        }
+
+        txtScore.GetComponent<Text>().text = "Score: " + m_score;
+        
+    }
+}
---
+++ Assets/Code/ScoreTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTarget : MonoBehaviour
+{
+    [SerializeField]
+    private int m_scoreValue = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            Debug.Log("Hit");
+            ScoreManager.instance.AddToScore(m_scoreValue);
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
