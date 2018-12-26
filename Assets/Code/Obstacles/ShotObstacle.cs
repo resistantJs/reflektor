c27fb508b62729c96cb12e1a1ed19a9e5cc2ea42
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2018-12-26 16:23:14 +0000

    Added new obstacle type: ShotObstacle - restores one bounce to the projectile that strikes it, being destroyed after doing so

---
+++ Assets/Code/Obstacles/ShotObstacle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ShotObstacle : MonoBehaviour, IObstacle
+{
+    public void TriggerObstacleEffect(GameObject _projectile)
+    {
+        _projectile.GetComponent<Projectile>().RestoreBounces(1);
+
+        Debug.Log("ShotObstacle: Restored one bounce");
+
+        Destroy(gameObject);
+    }
+}
